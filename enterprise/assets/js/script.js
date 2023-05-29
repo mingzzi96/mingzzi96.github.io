820201f811b5a7a58730d282f18f9429a0d1490c
mingming <jimin23623@gmail.com>
2023-05-29 16:32:35 +0900

    Added JS functions and started showcase section

--- enterprise/assets/js/script.js
+++ enterprise/assets/js/script.js
@@ -1,4 +1,21 @@
-function Vidload() {
-    console.log("loaded!")
+// video 로드 확인하여 이미지 opacity 0으로 만들어주기
+function videoLoad() {
+    console.log("video loaded!")
     document.querySelector(".intro picture").classList.add("loaded")
 }
+
+// lang menu
+let langChangeBtn = document.querySelector(".header__lang");
+let langChangeSubNav = document.querySelector(".header__lang--subnav");
+langChangeBtn.addEventListener("click", function(){
+    langChangeSubNav.classList.toggle("show");
+})
+
+// header 특정 위치 이상부터에서만 보이도록
+window.addEventListener("scroll", function(){
+    if(document.querySelector("html").scrollTop > 500){
+        document.querySelector("header").classList.remove("show")
+    }else {
+        document.querySelector("header").classList.add("show")
+    }
+})
