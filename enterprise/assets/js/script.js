870e46a0526cdb36a001e7642b01ed424f6b9390
mingming <jimin23623@gmail.com>
2023-05-29 19:26:12 +0900

    publishing is done except footer

--- enterprise/assets/js/script.js
+++ enterprise/assets/js/script.js
@@ -14,8 +14,44 @@ langChangeBtn.addEventListener("click", function(){
 // header 특정 위치 이상부터에서만 보이도록
 window.addEventListener("scroll", function(){
     if(document.querySelector("html").scrollTop > 500){
-        document.querySelector("header").classList.remove("show")
-    }else {
         document.querySelector("header").classList.add("show")
+    }else {
+        document.querySelector("header").classList.remove("show")
     }
+})
+
+// header white/black 효과 넣어주기
+function headerColorChange() {
+
+    window.addEventListener("scroll", function(){
+        let htmlTop = document.querySelector("html").scrollTop;
+        let challengeTop = document.querySelector(".challenge").offsetTop;
+        let possibleTop = document.querySelector(".slide--possibility").offsetTop;
+        let challengeAssetTop = document.querySelector(".challenge--asset").offsetTop;
+        console.log(challengeAssetTop)
+        console.log(htmlTop)
+        
+        if(challengeTop <= htmlTop && htmlTop < possibleTop){
+            // section challenge보다 htmlTop이 더 클때
+            document.querySelector("header").classList.remove("white")
+            document.querySelector("body").classList.remove("dark")
+        }else if(challengeTop <= htmlTop && possibleTop <= htmlTop && htmlTop < challengeAssetTop) {
+            // section slide--possibility htmlTop이 더 클때
+            document.querySelector("header").classList.add("white")
+            document.querySelector("body").classList.add("dark")
+        }else if(challengeAssetTop <= htmlTop) {
+            // section challenge--asset htmlTop이 더 클때
+            document.querySelector("header").classList.remove("white")
+            document.querySelector("body").classList.remove("dark")
+        }else {
+            // 초기화
+            document.querySelector("header").classList.add("white")
+            document.querySelector("body").classList.remove("dark")
+        }
+    })
+}
+
+headerColorChange()
+window.addEventListener("resize", function(){
+    headerColorChange()
 })
