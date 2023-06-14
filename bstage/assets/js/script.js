b8a9518f2da13063714d0199fd580770e39f382f
mingming <jimin23623@gmail.com>
2023-06-15 01:14:06 +0900

    일단 반응형 전까지 완료

--- bstage/assets/js/script.js
+++ bstage/assets/js/script.js
@@ -22,4 +22,63 @@ $(function () {
             $('.header').removeClass('on')
         }
     });
+
+    // scroll image
+    const imageControll1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".owner",
+            start: "0% 0%",
+            end: "100% -100%",
+            // markers: true,
+            scrub: 1,
+        }
+    })
+
+    imageControll1
+        .to(".everything .introduce-scrollarea__left", { translateY : 120 })
+        .to(".everything .introduce-scrollarea__right", { translateY : -120 })
+
+    const imageControll2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".everything",
+            start: "0% 0%",
+            end: "100% -100%",
+            // markers: true,
+            scrub: 1,
+        }
+    })
+
+    imageControll2
+        .to(".communication .introduce-scrollarea__left", { translateY : 120 })
+        .to(".communication .introduce-scrollarea__right", { translateY : -120 })
+
+    
+
+    const imageControll3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".communication",
+            start: "0% 0%",
+            end: "100% -100%",
+            // markers: true,
+            scrub: 1,
+        }
+    })
+
+    imageControll3
+        .to(".income .introduce-scrollarea__left", { translateY : 120 })
+        .to(".income .introduce-scrollarea__right", { translateY : -120 })
+
+
+
+    // business-bg
+    ScrollTrigger.create({
+        trigger: ".business",
+        start: "-70% 0%",
+        end: "200% 180%",
+        // markers: true,
+        onEnter: () => {
+            $(".business-bg__left").addClass("on");
+        }
+    })
+
 })
