bbd8dfcb79214836bc188aca9498b50a12c9155d
mingming <jimin23623@gmail.com>
2023-06-14 17:38:50 +0900

    헤더 애니메이션 추가

--- bstage/assets/js/script.js
+++ bstage/assets/js/script.js
@@ -5,5 +5,16 @@ $(function () {
         },
         loop: true
     });
-    
+    // 스크롤 감지
+    $(window).on('mousewheel',function(e){
+        var wheel = e.originalEvent.wheelDelta;
+
+            //스크롤값을 가져온다.
+        if(wheel>0){
+            $('.header').removeClass('on')
+        } else {
+        //스크롤 내릴때
+            $('.header').addClass('on')
+        }
+    });
 })
