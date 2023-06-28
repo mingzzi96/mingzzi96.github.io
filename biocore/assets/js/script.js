e5ecbe5d5e279b6b0a7b64b717f8db27d642b7f2
mingming <jimin23623@gmail.com>
2023-06-29 02:26:02 +0900

    working on bio

---
+++ biocore/assets/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    let swiper = new Swiper(".mainSwiper", {
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+    });
+})
