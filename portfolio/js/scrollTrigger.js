bbf1a7dd0dec54f78607269ada927344a7eceacf
mingming <jimin23623@gmail.com>
2023-08-28 15:47:59 +0900

    디자인 레이아웃

--- portfolio/js/scrollTrigger.js
+++ portfolio/js/scrollTrigger.js
@@ -6,7 +6,7 @@ $(function () {
             start: "0% 0%", // 트리거 기준  / 윈도우 기준
             end: "100% 0%",
             // markers: true,
-            scrub: 1, // 문대기 없애줌
+            scrub: 0, // 문대기 없애줌
         },
         default: {
             ease: "none" // ease 초기화해서 일정하게 들어가게끔
@@ -19,5 +19,29 @@ $(function () {
         .to(".intro h1:nth-child(3)", { x: -90 + "%" }, "a")
         .to(".intro h1:nth-child(2)", { x: 90 + "%" }, "a")
         .to(".intro h1:nth-child(4)", { x: 90 + "%" }, "a")
+
+
+
+
+
+    const moveExTextMove = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".move.ex",
+            start: "-40% 0%", // 트리거 기준  / 윈도우 기준
+            end: "100% 0%",
+            // markers: true,
+            scrub: 0, // 문대기 없애줌
+        },
+        default: {
+            ease: "none" // ease 초기화해서 일정하게 들어가게끔
+        }
+    })
+
+    moveExTextMove
+        .addLabel("a") // 같이 실행되고 싶은곳에만 추가
+        .to(".move.ex h2:nth-child(1)", { x: 0 + "%", opacity:1 }, "a")
+        .to(".move.ex h2:nth-child(3)", { x: 0 + "%", opacity:1 }, "a")
+        .to(".move.ex h2:nth-child(2)", { x: 0 + "%", opacity:1 }, "a")
+        // .to(".move.ex h2", { opacity: 0 })
 
 })
