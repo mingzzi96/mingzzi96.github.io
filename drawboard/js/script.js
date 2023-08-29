164bd71a2cc228b793208a0a9115876db1ad3f29
mingming <jimin23623@gmail.com>
2023-08-29 15:35:31 +0900

    그림판 업데이트

--- drawboard/js/script.js
+++ drawboard/js/script.js
@@ -1,95 +1,199 @@
 class DrawingBoard {
-    MODE = "NONE"; // NONE BRUSH EARASER
-    isMouseDown = false;
-    constructor() {
-      this.assignElement();
-      this.initContext();
-      this.addEvent();
+  MODE = "NONE"; // NONE BRUSH ERASER
+  isMouseDown = false;
+  eraserColor = "#FFFFFF";
+  backgroundColor = "#FFFFFF";
+  undoArray = [];
+
+  constructor() {
+    this.assignElement();
+    this.initContext();
+    this.initCanvasBackgroundColor();
+    this.addEvent();
+  }
+
+  assignElement() {
+    this.containerEl = document.getElementById("container");
+    this.canvasEl = this.containerEl.querySelector("#canvas");
+    this.toolbarEl = this.containerEl.querySelector("#toolbar");
+    this.brushEl = this.toolbarEl.querySelector("#brush");
+    this.colorPickerEl = this.toolbarEl.querySelector("#colorPicker");
+    this.brushPannelEl = this.containerEl.querySelector("#brushpannel");
+    this.brushSize = this.brushPannelEl.querySelector("#brushSize");
+    this.brushSizePreviewEl =
+      this.brushPannelEl.querySelector("#brushSizePreview");
+    this.eraserEl = this.toolbarEl.querySelector("#eraser");
+    this.navigatorEl = this.toolbarEl.querySelector("#navigator");
+    this.navigatorImageContainerEl = this.containerEl.querySelector("#imgNav");
+    this.navigatorImageEl =
+      this.navigatorImageContainerEl.querySelector("#canvasImg");
+    this.undoEl = this.toolbarEl.querySelector("#undo");
+    this.clearEl = this.toolbarEl.querySelector("#clear");
+    this.downloadLinkEl = this.toolbarEl.querySelector("#download");
+  }
+
+  initContext() {
+    this.context = this.canvasEl.getContext("2d");
+  }
+
+  initCanvasBackgroundColor() {
+    this.context.fillStyle = this.backgroundColor;
+    this.context.fillRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+  }
+
+  addEvent() {
+    this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+    this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+    this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
+    this.canvasEl.addEventListener("mouseout", this.onMouseOut.bind(this));
+    this.brushSize.addEventListener("input", this.onChangeBrushSize.bind(this));
+    this.colorPickerEl.addEventListener("input", this.onChangeColor.bind(this));
+    this.eraserEl.addEventListener("click", this.onClickEraser.bind(this));
+    this.navigatorEl.addEventListener(
+      "click",
+      this.onClickNavigator.bind(this)
+    );
+    this.undoEl.addEventListener("click", this.onClickUndo.bind(this));
+    this.clearEl.addEventListener("click", this.onClickClear.bind(this));
+    this.downloadLinkEl.addEventListener(
+      "click",
+      this.onClickDownloadLink.bind(this)
+    );
+  }
+
+  onClickDownloadLink() {
+    this.downloadLinkEl.href = this.canvasEl.toDataURL("image/jpeg", 1);
+    this.downloadLinkEl.download = "example.jpeg";
+  }
+
+  onClickClear() {
+    this.context.clearRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+    this.undoArray = [];
+    this.updateNavigator();
+    this.initCanvasBackgroundColor();
+  }
+
+  onClickUndo() {
+    if (this.undoArray.length === 0) {
+      alert("더이상 실행 취소 할 수 없습니다.");
+      return;
     }
-  
-    assignElement() {
-      this.containerEl = document.getElementById("container");
-      this.canvasEl = this.containerEl.querySelector("#canvas");
-      this.toolbarEl = this.containerEl.querySelector("#toolbar");
-      this.brushEl = this.toolbarEl.querySelector("#brush");
-      this.colorPickerEl = this.toolbarEl.querySelector("#colorPicker");
-      this.brushPannelEl = this.containerEl.querySelector("#brushpannel");
-      this.brushSize = this.brushPannelEl.querySelector("#brushSize");
-      this.brushSizePreviewEl =
-        this.brushPannelEl.querySelector("#brushSizePreview");
+    let previousDataUrl = this.undoArray.pop();
+    let previousImage = new Image();
+    previousImage.onload = () => {
+      this.context.clearRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+      this.context.drawImage(
+        previousImage,
+        0,
+        0,
+        this.canvasEl.width,
+        this.canvasEl.height,
+        0,
+        0,
+        this.canvasEl.width,
+        this.canvasEl.height
+      );
+    };
+    previousImage.src = previousDataUrl;
+    this.updateNavigator();
+  }
+
+  saveState() {
+    if (this.undoArray.length > 4) {
+      this.undoArray.shift(); // 제일 앞 값을 빼준다.
+      this.undoArray.push(this.canvasEl.toDataURL());
+    } else {
+      this.undoArray.push(this.canvasEl.toDataURL());
     }
-  
-    initContext() {
-      this.context = this.canvasEl.getContext("2d");
-    }
-  
-    addEvent() {
-      this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
-      this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
-      this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
-      this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
-      this.canvasEl.addEventListener("mouseout", this.onMouseOut.bind(this));
-      this.brushSize.addEventListener("input", this.onChangeBrushSize.bind(this));
-      this.colorPickerEl.addEventListener("input", this.onChangeColor.bind(this));
-    }
-  
-    onMouseOut() {
-      if (this.MODE === "NONE") return;
-      this.isMouseDown = false;
-    }
-  
-    onChangeColor(event) {
-      this.brushSizePreviewEl.style.background = event.target.value;
-    }
-  
-    onChangeBrushSize(event) {
-      this.brushSizePreviewEl.style.width = `${event.target.value}px`;
-      this.brushSizePreviewEl.style.height = `${event.target.value}px`;
-    }
-  
-    onMouseDown(event) {
-      if (this.MODE === "NONE") return;
-      this.isMouseDown = true;
-      const currentPosition = this.getMousePosition(event);
-      this.context.beginPath();
-      this.context.moveTo(currentPosition.x, currentPosition.y);
-      this.context.lineCap = "round";
+    console.log(this.undoArray.length);
+  }
+
+  onClickNavigator(event) {
+    this.isNavigatorVisible = !event.currentTarget.classList.contains("active");
+    event.currentTarget.classList.toggle("active");
+    this.navigatorImageContainerEl.classList.toggle("hide");
+    this.updateNavigator();
+  }
+
+  updateNavigator() {
+    if (!this.isNavigatorVisible) return;
+    this.navigatorImageEl.src = this.canvasEl.toDataURL();
+  }
+
+  onClickEraser(event) {
+    const isActive = event.currentTarget.classList.contains("active");
+    // brush div 안에 i 태그가 눌릴 수 있으니까 currentTarget으로 찾기.
+    this.MODE = isActive ? "NONE" : "ERASER";
+    this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
+    this.brushPannelEl.classList.add("hide");
+    event.currentTarget.classList.toggle("active");
+    this.brushEl.classList.remove("active");
+  }
+
+  onMouseOut() {
+    if (this.MODE === "NONE") return;
+    this.isMouseDown = false;
+    this.updateNavigator();
+  }
+
+  onChangeColor(event) {
+    this.brushSizePreviewEl.style.background = event.target.value;
+  }
+
+  onChangeBrushSize(event) {
+    this.brushSizePreviewEl.style.width = `${event.target.value}px`;
+    this.brushSizePreviewEl.style.height = `${event.target.value}px`;
+  }
+
+  onMouseDown(event) {
+    if (this.MODE === "NONE") return;
+    this.isMouseDown = true;
+    const currentPosition = this.getMousePosition(event);
+    this.context.beginPath();
+    this.context.moveTo(currentPosition.x, currentPosition.y);
+    this.context.lineCap = "round";
+    if (this.MODE === "BRUSH") {
       this.context.strokeStyle = this.colorPickerEl.value;
       this.context.lineWidth = this.brushSize.value;
-      // this.context.lineTo(400, 400);
-      // this.context.stroke();
+    } else if (this.MODE === "ERASER") {
+      this.context.strokeStyle = this.eraserColor;
+      this.context.lineWidth = 50;
     }
-  
-    onMouseMove(event) {
-      // 마우스가 다운된 상태가 아니라면 동작 멈춰.
-      if (!this.isMouseDown) return;
-      const currentPosition = this.getMousePosition(event);
-      this.context.lineTo(currentPosition.x, currentPosition.y);
-      this.context.stroke();
-    }
-  
-    onMouseUp() {
-      if (this.MODE === "NONE") return;
-      this.isMouseDown = false;
-    }
-  
-    getMousePosition(event) {
-      const boundaries = this.canvasEl.getBoundingClientRect();
-      return {
-        x: event.clientX - boundaries.left,
-        y: event.clientY - boundaries.top,
-      };
-    }
-  
-    onClickBrush(event) {
-      const isActive = event.currentTarget.classList.contains("active");
-      // brush div 안에 i 태그가 눌릴 수 있으니까 currentTarget으로 찾기.
-      this.MODE = isActive ? "NONE" : "BRUSH";
-      this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
-      this.brushPannelEl.classList.toggle("hide");
-      this.brushEl.classList.toggle("active");
-    }
+    this.saveState();
   }
-  
-  new DrawingBoard();
-  
+
+  onMouseMove(event) {
+    // 마우스가 다운된 상태가 아니라면 동작 멈춰.
+    if (!this.isMouseDown) return;
+    const currentPosition = this.getMousePosition(event);
+    this.context.lineTo(currentPosition.x, currentPosition.y);
+    this.context.stroke();
+  }
+
+  onMouseUp() {
+    if (this.MODE === "NONE") return;
+    this.isMouseDown = false;
+    this.updateNavigator();
+  }
+
+  getMousePosition(event) {
+    const boundaries = this.canvasEl.getBoundingClientRect();
+    return {
+      x: event.clientX - boundaries.left,
+      y: event.clientY - boundaries.top,
+    };
+  }
+
+  onClickBrush(event) {
+    const isActive = event.currentTarget.classList.contains("active");
+    // brush div 안에 i 태그가 눌릴 수 있으니까 currentTarget으로 찾기.
+    this.MODE = isActive ? "NONE" : "BRUSH";
+    this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
+    this.brushPannelEl.classList.toggle("hide");
+    event.currentTarget.classList.toggle("active");
+    this.eraserEl.classList.remove("active");
+  }
+}
+
+new DrawingBoard();
