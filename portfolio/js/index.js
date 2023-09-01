d05a2c25ac18bedd567a3f663acd6ecfe6b32618
mingming <jimin23623@gmail.com>
2023-09-01 19:46:31 +0900

    포폴 수정중

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -52,3 +52,16 @@ hoverEyeRed.forEach( (e) => {
     })
 
 } )
+
+// 스크롤 감지
+window.addEventListener('wheel',(event) => {
+	let wheel = event.wheelDeltaY;
+  
+	if(wheel > 0) {
+        document.querySelector(".header").classList.add("show");
+    }
+	else { // (wheel < 0)
+        document.querySelector(".header").classList.remove("show");
+    }
+	preScrollTop = nextScrollTop;
+});
