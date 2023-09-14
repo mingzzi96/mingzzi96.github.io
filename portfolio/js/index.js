c6eb8680e34e885ca53fae241d91c66cbd2750dc
mingming <jimin23623@gmail.com>
2023-09-14 19:41:47 +0900

    모바일 감지

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,59 +1,60 @@
 
-
+const isMobile = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)
 const customedCursor = document.querySelector(".cursor");
 const customedCursorRed = document.querySelector(".cursor-red");
 const hoverEye = document.querySelectorAll(".hover-eye");
 const hoverEyeRed = document.querySelectorAll(".hover-eye-red");
 
 
-
-document.addEventListener("mousemove", (e) => { // mousemove이벤트를 이용해 움
-
-    // 마우스의 좌표는 clientX와 clientY를 이용해 알수 있다. -> 브라우저 window의 좌표값 위치를 전달한다.
-
-    // pageX, pageY와는 다름.
-
-    const mouseX = e.clientX;
-
-    const mouseY = e.clientY;
-
-    customedCursor.style.left = mouseX + 'px';
-
-    customedCursor.style.top = mouseY + 'px';
-
-    customedCursorRed.style.left = mouseX + 'px';
-
-    customedCursorRed.style.top = mouseY + 'px';
-
-});
-
-hoverEye.forEach( (e) => {
-    e.addEventListener("mouseenter", (event) => {
-        customedCursor.classList.add("show");
-        e.classList.add("cursor-none");
-    })
-
-    e.addEventListener("mouseleave", (event) => {
-        customedCursor.classList.remove("show");
-        e.classList.remove("cursor-none");
-    })
-
-} )
-
-hoverEyeRed.forEach( (e) => {
-    e.addEventListener("mouseenter", (event) => {
-        customedCursorRed.classList.add("show");
-        e.classList.add("cursor-none");
-        console.log("e :"+e);
-        // console.log("e :"+e.target);
-    })
-
-    e.addEventListener("mouseleave", (event) => {
-        customedCursorRed.classList.remove("show");
-        e.classList.remove("cursor-none");
-    })
-
-} )
+if(isMobile){
+    document.addEventListener("mousemove", (e) => { // mousemove이벤트를 이용해 움
+    
+        // 마우스의 좌표는 clientX와 clientY를 이용해 알수 있다. -> 브라우저 window의 좌표값 위치를 전달한다.
+    
+        // pageX, pageY와는 다름.
+    
+        const mouseX = e.clientX;
+    
+        const mouseY = e.clientY;
+    
+        customedCursor.style.left = mouseX + 'px';
+    
+        customedCursor.style.top = mouseY + 'px';
+    
+        customedCursorRed.style.left = mouseX + 'px';
+    
+        customedCursorRed.style.top = mouseY + 'px';
+    
+    });
+    
+    hoverEye.forEach( (e) => {
+        e.addEventListener("mouseenter", (event) => {
+            customedCursor.classList.add("show");
+            e.classList.add("cursor-none");
+        })
+    
+        e.addEventListener("mouseleave", (event) => {
+            customedCursor.classList.remove("show");
+            e.classList.remove("cursor-none");
+        })
+    
+    } )
+    
+    hoverEyeRed.forEach( (e) => {
+        e.addEventListener("mouseenter", (event) => {
+            customedCursorRed.classList.add("show");
+            e.classList.add("cursor-none");
+            console.log("e :"+e);
+            // console.log("e :"+e.target);
+        })
+    
+        e.addEventListener("mouseleave", (event) => {
+            customedCursorRed.classList.remove("show");
+            e.classList.remove("cursor-none");
+        })
+    
+    } )
+}
 
 // 스크롤 감지
 window.addEventListener('wheel',(event) => {
